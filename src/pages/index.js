1f955762d7d366636820edb5b54bb414a73175d5
Yujohn Nattrass <yujohn.natt@gmail.com>
2021-11-02 13:45:18 -0700

    update h1

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = ({ name }) => {
   return (
     <main>
       <title>Home Page</title>
-      <h1>Welcome to my site v2!!</h1>
+      <h1>Changes with stagehand</h1>
       <Link to="/about">{name}</Link>
       <p>Hello world</p>
     </main>
